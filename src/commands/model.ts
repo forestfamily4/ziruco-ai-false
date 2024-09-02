af67589ec443e81c069dd41824adb9f6ba362833
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2024-09-02 13:50:41 +0900

    ニックネーム

--- src/commands/model.ts
+++ src/commands/model.ts
@@ -2,7 +2,7 @@ import type { Message } from "discord.js";
 import { Lang, run } from "../lib/compile";
 import { Bot } from "../bot";
 import { collection } from "../lib/db";
-import { models } from "../ai/api";
+import { Model, models } from "../ai/api";
 
 export const name = "model";
 
@@ -11,6 +11,16 @@ export const description = "modelを確認";
 export const aliases = [];
 
 export const usages = ["[コード]"];
+
+const nicknames: Map<Model, string> = new Map([
+  ["gpt-4o-mini", "ジルコGPTカス"],
+  ["gpt-4o", "ジルコGPT"],
+  ["meta-llama-3.1-405b-instruct", "ジルコラマ405号"],
+  ["Phi-3-medium-128k-instruct", "ジルコファイさん"],
+  ["meta-llama-3.1-70b-instruct", "ジルコラマ70号"],
+  ["cohere-command-r-plus", "ジルコヒア"],
+  ["Mistral-large", "ジルコストラル"]
+]);
 
 export async function exec(
   message: Message,
@@ -31,6 +41,8 @@ export async function exec(
         { upsert: true },
       );
       message.reply(`${model}を使用します`);
+      const nickname = nicknames.get(model as Model);
+      nickname && message.guild?.members.me?.setNickname(nickname);
     } else {
       message.reply(
         `使用可能なモデルは${models.map((s) => s.toString()).join(", ")}です`,
